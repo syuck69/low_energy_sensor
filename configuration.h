bbb4037ef97d2583c3efe52d310bc4b4d8d0d555
syuck69 <113301348+syuck69@users.noreply.github.com>
2022-10-22 10:16:19 +0200

    Update configuration.h

--- configuration.h
+++ configuration.h
@@ -9,9 +9,9 @@
  *   Wiring (ATtiny85):
  *                             +-------+
  *                            1|*      |8  VCC
- *                            2|       |7  SCL (BME280)
- *   Data TX RF 433Mhz - PB4  3|       |6
- *                       GND  4|       |5  SDA (BME280)
+ *                            2|       |7  SCL (SHT3X)
+ *   Data TX RF 433Mhz - PB4  3|       |6  PB1 VCC to RF 433Mhz
+ *                       GND  4|       |5  SDA (SHT3X)
  *                             +-------+
 */
   
@@ -23,14 +23,14 @@
  * Choose your mode
  * 0 : Temperature                       [THN132N]
  * 1 : Temperature + Humidity            [THGR122N]
- * 2 : Temperature + Humidity + Pressure [BHTR968]
+
 */ 
  
-#define MODE                    (2)
+#define MODE                    (1)
 
 /* Roughly, how many SECONDS should I sleep between 2 transmissions ? */
 
-#define SLEEPING_TIME_S         (20)
+#define SLEEPING_TIME_S         (60)
 
 /* 
  * Should I ALWAYS send messages or ONLY NEW message ? 
@@ -64,152 +64,38 @@
  * You could safely keep this as is
 */
 
-#define CHANNEL                 (0x20)
+#define CHANNEL                 (0x10)
 
 /* 
  * ID
  * Each sensor must have a unique ID
  * eg:
- * - first sensor:  0xCC
- * - second sensor: 0xCD
- * - third sensor:  0xCE
- * - fourth sensor: 0xCF
+ * - first sensor:  0xCC 204
+ * - second sensor: 0xCD 205
+ * - third sensor:  0xCE 206
+ * - fourth sensor: 0xCF 207
  * - fifth sensor:  0xD0
  * - etc...
 */
 
-#define ID                      (0xCC)
+#define ID                      (0xCA)
 
 
 /***
- * BME280 PARAMETERS
+ * SHT3X PARAMETERS
  */
  
 /* 
  * I2C address of the device
- * If your BME280 doesn't work with this address, try 0x77
+ * If you want to use address 0x45 set the appropriate pin to high
 */
 
-#define BME280_I2C_ADDR         (0x76)
+#define SHT3X_I2C_ADDR         (0x44)
 
-/* If needed, you can adjust the temperature in °C returned by the sensor */
+/* If needed, you can adjust the temperature in °C and the Relative Humidity % returned by the sensor */
 
-#define ADJUST_TEMPERATURE_C    (-0.7)
+#define ADJUST_TEMPERATURE_C    (-0.5)
+#define ADJUST_RELATIVEHUMIDITY    (-3.0)
 
-/* BME280 Configuration (oversampling, filter & mode)
- * This should be fine for most usage in our Low Energy quest.
- * Choices are:
- *  - Forced mode (take a measurement and the chip returns to sleep)
- *  - No filter
- *  - Oversammpling x1 for temperature, humidity and pressure
- * These choices are those recommended by Bosch Sensortec in their datasheet
- * for our use (weather station: minimal power consumption)
- * Some brief explanations are below if you want to modify these default settings
-*/
 
-#define BME280_CTRL_CONFIG      (0b00000000)
-#if MODE == 0
-#define BME280_CTRL_HUM         (0b00000000)
-#define BME280_CTRL_MEAS        (0b00100001)
-#elif MODE == 1
-#define BME280_CTRL_HUM         (0b00000001)
-#define BME280_CTRL_MEAS        (0b00100001)
-#else
-#define BME280_CTRL_HUM         (0b00000001)
-#define BME280_CTRL_MEAS        (0b00100101)
-#endif
-
-/********************************************/
-/********* END USER CONFIGURATION ***********/
-/********************************************/
-
-/* What is BME280_CTRL_CONFIG ?
- * --------------------------
- * CONFIG (RATE - FILTER - INTERFACE OPTIONS OF THE DEVICE)   
- * cf DS 5.4.6
- *
- * t_sb : STANDBY TIME IN NORMAL MODE
- * 000 :    0.5 ms
- * 110 :   10   ms
- * 111 :   20   ms
- * 001 :   62.5 ms
- * 010 :  125   ms
- * 011 :  250   ms
- * 100 :  500   ms
- * 101 : 1000   ms
- *
- * filter : FILTER COEFFICIENT
- * 000 : off
- * 001 :  2
- * 010 :  4
- * 011 :  8
- * 100 : 16
- *
- * config : Register 0xF5
- * byte = t_sb[2:0] : filter[2:0] : not used[0] : spi3w_en[0]
- *
- * #define BME280_CTRL_CONFIG  0b00000000
- *                               ------ -
- *                                 |  | |
- *                                 |  | -> spi3w_en (1 bit)
- *                                 |  ---> Filter coefficient (3 bits)
- *                                 ------> Stanby time in Normal Mode (t_sb: 3 bits)
- * 
- * 
- *
- * What is BME280_CTRL_HUM ?
- * -----------------------
- * HUMIDITY (osrs_h)
- * cf DS 5.4.3
- *  
- * OVERSAMPLING (humidity)
- * 000 : skipped
- * 001 : oversampling x1
- * 010 : oversampling x2
- * 011 : oversampling x4
- * 100 : oversampling x8
- * 101 : oversampling x16
- * 
- * ctrl_hum : Register 0xF2
- * byte = empty[4:0] : osrs_h[2:0]
- * note: changes to this register only become effective after a WRITE operation to ctrl_meas
- *
- * #define BME280_CTRL_HUM  0b00000001
- *                                 ---
- *                                   |
- *                                   -> Humidity oversampling (osrs_h: 3 bits)
- *                                   
- *
- *
- *
- * What is BME280_CTRL_MEAS ?
- * ------------------------
- * TEMPERATURE - PRESSION - MODE (osrs_t, osrs_p, mode)
- * cf DS 5.4.5
- *
- * OVERSAMPLING (temperature / pression)
- * 000 : skipped
- * 001 : oversampling x1
- * 010 : oversampling x2
- * 011 : oversampling x4
- * 100 : oversampling x8
- * 101 : oversampling x16
- * 
- * DEVICE MODE
- * 00       : sleep
- * 01 or 10 : forced
- * 11       : normal
- *
- * ctrl_meas : Register 0xF4
- * byte = osrs_t[2:0] : osrs_p[2:0] : mode[1:0]
- *
- * #define BME280_CTRL_MEAS  0b00100101
- *                             --------
- *                               T  P M
- *                               |  | |
- *                               |  | -> Mode (2 bits)
- *                               |  ---> Pressure oversampling (osrs_p: 3 bits)
- *                               ------> Temperature oversampling (osrs_t: 3 bits)
-*/ 
-
-#endif
+#
